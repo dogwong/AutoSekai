9ae728757beced9142fbcb412be2f38f87ec5f91
dogwong <dogwong@users.noreply.github.com>
2021-03-07 22:58:13 +0800

    restructure

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-// imports
+// import node native stuffs
 const os = require('os')
 const fs = require('fs')
-const SusAnalyzer = require('sus-analyzer')
-const ScorePlayer = require('./lib/score-player')
 const readline = require('readline');
+
+// import npm and ./lib stuffs
+const SusAnalyzer = require('sus-analyzer')
+const SekaiSusReader = require('./lib/sekai-sus-reader')
+const Toucher = require('./lib/toucher')
+const ScorePlayer = require('./lib/score-player')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: 'AutoPlay> '
 });
-// touch stuffs
-const Touch = require('./lib/touch')
-let BleTouch;
-let bleTouch;
 const sleep = require('util').promisify(setTimeout)
 
-// init
-if (os.platform() == "linux") {
-  console.log("linux! init bluetooth");
-  
-  BleTouch = require('./lib/bleTouch');
-  bleTouch = new BleTouch();
-} else {
-  console.log("not linux!");
-}
-let touch = new Touch(bleTouch);
+
+
 
 // config
 const FLICK_EARLY = 20; // ms earlier to preform flick
 const SLIDE_UPDATE_FREQUENCY = 30; // ms touch update interval during slide
 
-// https://pjsek.ai/assets/startapp/music/music_score
+// score files source: https://pjsek.ai/assets/startapp/music/music_score
 
 (async () => {
   // init stuffs
@@ -80,7 +72,7 @@ const player = new ScorePlayer()
       }
       let i = 0
       function loopTouch () {
-        sendTouch([touch])
+        Toucher.SendTouch([touch])
         i += 1
         if (i < 26)
           setTimeout(loopTouch, 100)
@@ -92,20 +84,20 @@ const player = new ScorePlayer()
         y: Math.round(1650 / 2340 * 10000),
         t: 50
       }
-      sendTouch([touch])
+      Toucher.SendTouch([touch])
     } else if (key.sequence == "t") {
-      sendTouch([{
+      Toucher.SendTouch([{
         x: Math.round((1080 - 850) / 1080 * 10000),
         y: Math.round(1000 / 2340 * 10000),
         t: 2000
       }])
-      sendTouch([{
+      Toucher.SendTouch([{
         x: Math.round((1080 - 850) / 1080 * 10000),
         y: Math.round(1340 / 2340 * 10000),
         t: 2000
       }])
     } else if (key.sequence == "y") {
-      sendTouch([{
+      Toucher.SendTouch([{
         x: Math.round((1080 - 850) / 1080 * 10000),
         y: Math.round(1000 / 2340 * 10000),
         t: 200
@@ -139,7 +131,7 @@ const player = new ScorePlayer()
         t: 200
       }])
       await sleep(100)
-      sendTouch([{
+      Toucher.SendTouch([{
         x: Math.round((1080 - 850) / 1080 * 10000),
         y: Math.round(1340 / 2340 * 10000),
         t: 2000
@@ -154,7 +146,7 @@ const player = new ScorePlayer()
   });
 
   async function clickTillNextSong (loop = false) {
-    sendTouch([{
+    Toucher.SendTouch([{
       t: 50,
       x: Math.round((1080 - 1000) / 1080 * 10000),
       y: Math.round(2000 / 2340 * 10000),
@@ -162,7 +154,7 @@ const player = new ScorePlayer()
     console.log("Click Start!");
     await sleep(8000);
     // for (let i = 0; i < 60; i++) {
-    //   sendTouch([{
+    //   Toucher.SendTouch([{
     //     t: 30,
     //     x: Math.round((100 - 1000) / 1080 * 10000),
     //     y: Math.round(2250 / 2340 * 10000),
@@ -172,7 +164,7 @@ const player = new ScorePlayer()
     let i = 0;
     console.log("Loop click pause!");
     async function loopTouch () {
-      sendTouch([{
+      Toucher.SendTouch([{
         t: 1,
         x: Math.round((1080 - 100) / 1080 * 10000),
         y: Math.round(2250 / 2340 * 10000),
@@ -183,7 +175,7 @@ const player = new ScorePlayer()
       else {
         await sleep(100);
         console.log("Click resume!");
-        sendTouch([{
+        Toucher.SendTouch([{
           t: 50,
           x: Math.round((1080 - 680) / 1080 * 10000),
           y: Math.round(1550 / 2340 * 10000),
@@ -203,27 +195,27 @@ const player = new ScorePlayer()
             }
             // let i = 0
             // function loopTouch () {
-            //   sendTouch([touch])
+            //   Toucher.SendTouch([touch])
             //   i += 1
             //   if (i < 26)
             //     setTimeout(loopTouch, 100)
             // }
             // loopTouch()
             for (let i = 0; i < 28; i++) {
-              sendTouch([touch])
+              Toucher.SendTouch([touch])
               await sleep(100)
             }
             // await sleep(2600)
             // ------------- click song selection screen
             await sleep(1000)
-            sendTouch([{
+            Toucher.SendTouch([{
               x: Math.round((1080 - 1000) / 1080 * 10000),
               y: Math.round(1650 / 2340 * 10000),
               t: 50
             }])
             await sleep(500)
             // click select song
-            sendTouch([{
+            Toucher.SendTouch([{
               x: Math.round((1080 - 980) / 1080 * 10000),
               y: Math.round(2000 / 2340 * 10000),
               t: 50
@@ -240,10 +232,7 @@ const player = new ScorePlayer()
   }
 
 
-  if (os.platform() == "linux") {
-    console.log("init bluetooth");
-    await bleTouch.init();
-  }
+
 
   // "fillerSec": 9
   // const sus = fs.readFileSync('music_score/0047_normal.sus', 'utf8') // メルト
@@ -291,7 +280,7 @@ const player = new ScorePlayer()
   //   fs.writeFileSync(`score_0074_expert.png` , image)
   // })
 
-  let processedSus = readSusFile(sus)
+  let processedSus = SekaiSusReader.Read(sus)
   let susMsNotes = processedSus.timestampNotes
   let susMsSlides = processedSus.slides
 
@@ -350,7 +339,7 @@ const player = new ScorePlayer()
         ))
 
         if (waypoints.length === 0) {
-          sendTouch([{
+          Toucher.SendTouch([{
             x: screenXPositions, 
             y: Math.round((laneXPositions[note.lane - 2] + laneXPositions[note.lane + note.width - 2 - 1]) / 2),
             t: 10
@@ -367,7 +356,7 @@ const player = new ScorePlayer()
             t: 8
           })
         }
-        sendTouch(touchesToSend)
+        Toucher.SendTouch(touchesToSend)
       } else if (note.type == "slide head") {
         // console.log(`slide lane ${note.lane}`)
 
@@ -444,274 +433,17 @@ const player = new ScorePlayer()
           }
         }
 
-        sendTouch(touchesToSend)
+        Toucher.SendTouch(touchesToSend)
       }
     })
-    
+
+    console.log(payload.t, payload.objects.map(x => {
+      return `${x.type} ${x.lane}`
+    }));
   }
 })();
 setInterval(() => {}, 1000); // keep this script running
-// @param {{x: Number, y: Number, t: Number}[]} touches - An array of touch objects
-// * @type {Object}
 
-let touching = [false, false]
-let touchQueue = []
-/**
- * 
- * @typedef Touch
- * @property {Number} x - x coordinate
- * @property {Number} y - y coordinate
- * @property {Number} t - milliseconds of touch down duration
- * 
- * @param {Touch[]} touches - An array of touch objects
- * @description Perform a series of action with a single touch
- */
-function sendTouch (touches) {
-  
-  // array of touch object: x, y, t
-  // console.log(`sendTouch len = ${touch.length}`);
-  touches.x = Math.round(touches.x + Math.random() * 5 - 2)
-  touches.y = Math.round(touches.y + Math.random() * 5 - 2)
-  touchQueue.push(touches)
-  // setTimeout(() => {
-    processTouchQueue()
-  // }, 0)
-}
-
-function processTouchQueue () {
-  if (touchQueue.length > 0) {
-    let freeSlot = touching[0] ? (touching[1] ? -1 : 1) : 0 //touching.indexOf(false)
-    // console.log(`freeSlot = ${freeSlot} - ${touching}`);
-    if (freeSlot >= 0) {
-      let actions = touchQueue.shift()
-      touching[freeSlot] = actions
-      
-
-      processTouch(touching[freeSlot])
-      async function processTouch (actions) {
-        touch.moveFingerTo(freeSlot, actions[0].x, actions[0].y)
-        // console.log("process touch " + freeSlot);
-        await sleep(actions[0].t)
-        actions.shift() // put shift at last to make sure moveFingerTo is executed first
-        if (actions.length > 0) {
-          processTouch(actions)
-        } else {
-          touch.releaseFinger(freeSlot)
-          touching[freeSlot] = false
-          processTouchQueue()
-        }
-      }
-    }
-  }
-}
-
-
-function readSusFile (sus) {
-  // ref: https://github.com/PurplePalette/pjsekai-score-doc/wiki/%E4%BD%9C%E6%88%90%E6%96%B9%E6%B3%95
-    
-  const data = SusAnalyzer.getScore(sus, 480)
-  //    number[]
-  const absMeasure = []
-  const absMs = []
-  // measure # -> tick count
-  {
-    // calculate escaped time by measure id
-    let prevTick = 0
-    let prevMs = 0
-    for (let i = 0; i <= data.measure; i++) {
-      let tick = prevTick + 480 * data.BEATs[i - 1] * (data.BPMs[0] / data.BPMs[i - 1])
-      let ms = prevMs + 60 / data.BPMs[i - 1] * 1000 * data.BEATs[i - 1] * (data.BPMs[0] / data.BPMs[i - 1])
-      if (i === 0) {
-        tick = 0
-        ms = 0
-      }
-      absMeasure.push(tick)
-      absMs.push(Math.floor(ms))
-      prevTick = tick
-      prevMs = ms
-    }
-  }
-  // console.log("absMeasure", absMeasure);
-  // console.log("absMs", absMs);
-
-  let result = []
-
-  // use to remove overlaping tap + flick or tap + slide head
-  let shortNotesMap = {}
-
-  data.shortNotes.forEach(note => {
-    let type = "unknown"
-    if (note.lane == 0 && note.noteType == 4) {
-      type = "skill"
-    } else if (note.lane == 15 && note.noteType == 2) {
-      type = "fever chance"
-    } else if (note.lane == 15 && note.noteType == 1) {
-      type = "fever"
-    } else if (note.lane >= 2 && note.lane <= 13 && note.noteType == 1) {
-      type = "tap"
-    } else if (note.lane >= 2 && note.lane <= 13 && note.noteType == 2) {
-      type = "yellow tap"
-    } else if (note.lane >= 2 && note.lane <= 13 && note.noteType == 3) {
-      type = "diamond" // combo point on slide, will not affect slide path
-      // chunithm = flick note
-      // When the flick notes of Chunithm are placed on the relay point or invisible relay point of the slide, the shape of the slide at the placed relay point is ignored and the relay points before and after are interpolated and connected.
-      // In the case of an image, a relay point is placed on the refracting slide, and flick notes are placed on it.
-      // Therefore, refraction is ignored and it is linear.
-      // If flick notes are placed above an invisible relay point, the relay point will not be drawn.
-    }
-    
-    let resultObject = {
-      t: Math.floor(absMs[note.measure] + note.tick / 480 * 60 / data.BPMs[note.measure] * 1000 * (data.BPMs[0] / data.BPMs[note.measure])),
-      type: type,
-      lane: note.lane,
-      raw: note,
-      measure: note.measure,
-      width: note.width,
-      r: "short", // raw type
-    }
-
-    shortNotesMap[`${resultObject.measure}_${resultObject.raw.tick}_${resultObject.lane}`] = resultObject
-    // will push to result after filtering out slide only
-  })
-
-  // use to map airNote to slideNote
-  let airNotesMap = {}
-
-  data.airNotes.forEach(note => {
-    let type = "unknown"
-    if (note.noteType == 5) { // air down (left)
-      type = "slide bend left"
-    } else if (note.noteType == 6) { // air down (right)
-      type = "slide bend right"
-    } else if (note.noteType == 2) { // air down (middle)
-      type = "slide bend middle"
-    } else if (note.noteType == 1) { // air up (middle)
-      type = "flick"
-    } else if (note.noteType == 4) { // air up (right)
-      type = "flick right"
-    } else if (note.noteType == 3) { // air up (left)
-      type = "flick left"
-    }
-
-    // for remove overlapping taps
-    let shortNote = shortNotesMap[`${note.measure}_${note.tick}_${note.lane}`]
-    if (shortNote) {
-      // console.log("remove shortNote", shortNote, "overlapping airNote", note);
-      delete shortNotesMap[`${note.measure}_${note.tick}_${note.lane}`]
-    } else {
-      shortNote = false
-    }
-
-    let resultObject = {
-      t: Math.floor(absMs[note.measure] + note.tick / 480 * 60 / data.BPMs[note.measure] * 1000 * (data.BPMs[0] / data.BPMs[note.measure])),
-      type: type,
-      lane: note.lane,
-      raw: note,
-      measure: note.measure,
-      width: note.width,
-      r: "air",
-      shortNote: shortNote,
-    }
-
-    airNotesMap[`${resultObject.measure}_${resultObject.raw.tick}_${resultObject.lane}`] = resultObject
-    // will push to result after filtering out slide only
-  })
-
-  let prevSlideId = 1
-  let slides = []
-
-  data.slideNotes.forEach(notes => {
-    let type = "unknown"
-    slides[prevSlideId] = []
-    for (let i = 0; i < notes.length; i++) {
-      const note = notes[i];
-      if (i == 0 && note.noteType == 1) {
-        type = "slide head"
-      } else if (i == notes.length - 1 && note.noteType == 2) {
-        type = "slide tail"
-      } else if (note.noteType == 3) {
-        type = "slide waypoint hvcombo" // have diamond
-      } else if (note.noteType == 5) {
-        type = "slide waypoint nocombo"
-      }
-
-      // for merge overlaying airNotes to slide
-      let airNote = airNotesMap[`${note.measure}_${note.tick}_${note.lane}`]
-      if (airNote) {
-        delete airNotesMap[`${note.measure}_${note.tick}_${note.lane}`]
-      } else {
-        airNote = false
-      }
-
-      // check overlapping taps
-      // expected taps type: yellow tap, diamond
-      let shortNote = shortNotesMap[`${note.measure}_${note.tick}_${note.lane}`]
-      let diamondNote = false;
-      if (shortNote) {
-        // console.log("remove shortNote", shortNote, "overlapping slideNote", note);
-        if (shortNote.type == "diamond") {
-          diamondNote = shortNote
-        } else {
-          delete shortNotesMap[`${note.measure}_${note.tick}_${note.lane}`]
-        }
-      } else {
-        shortNote = false
-      }
-
-      let resultObject = {
-        t: Math.floor(absMs[note.measure] + note.tick / 480 * 60 / data.BPMs[note.measure] * 1000 * (data.BPMs[0] / data.BPMs[note.measure])),
-        type: type,
-        lane: note.lane,
-        raw: notes,
-        measure: note.measure,
-        width: note.width,
-        r: "slide",
-        slideId: prevSlideId,
-        shortNote: shortNote,
-        airNote: airNote,
-        diamondNote: diamondNote,
-      }
-      slides[prevSlideId].push(resultObject)
-      result.push(resultObject)
-    }
-    slides[prevSlideId].sort((a, b) => {
-      return a.t - b.t
-    })
-
-    prevSlideId += 1
-  })
-  
-  // add remaining shortNotes to result
-  for (const key in shortNotesMap) {
-    if (Object.hasOwnProperty.call(shortNotesMap, key)) {
-      const note = shortNotesMap[key];
-      // console.log("push shortNotes", note);
-      result.push(note)
-    }
-  }
-  
-  // add remaining airNotes to result
-  // console.log("lonely airNotes", airNotesMap);
-  for (const key in airNotesMap) {
-    if (Object.hasOwnProperty.call(airNotesMap, key)) {
-      const note = airNotesMap[key];
-      // console.log("push airNote", note);
-      result.push(note)
-    }
-  }
-  
-
-  result = result.sort((a, b) => {
-    if (a.t == b.t)
-      return a.lane - b.lane
-    return a.t - b.t
-  })
-
-  return {
-    timestampNotes: result,
-    slides: slides,
-  }
-}
 
 
 console.log("Ready!");
